453b3c27941437f238e45a4ea6b1fc93c615e47d
vyking666 <46061972+vyking666@users.noreply.github.com>
2023-03-29 15:59:15 +0800

    react app created

---
+++ my-app/src/components/Instructions.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ems from './happy_woman.png';
+
+export class Instructions extends Component {
+  render() {
+    return(
+    <div>
+    <h1>Employee Mern Project</h1>
+    <p>This is an Employee Management System</p>
+    <img src={ems} alt="Happy girl in green sweater" />;
+      </div>
+    )
+  }
+}
+export default Instructions;
