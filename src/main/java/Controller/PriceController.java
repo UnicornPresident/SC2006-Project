c6c88a6a124801601db773acfda506aad818af2a
UnicornPresident <48112353+UnicornPresident@users.noreply.github.com>
2023-04-03 20:52:54 +0800

    Update PriceController.java

--- src/main/java/Controller/PriceController.java
+++ src/main/java/Controller/PriceController.java
@@ -9,6 +9,8 @@ import java.util.*;
 import java.time.*;
 import java.lang.*;
 import java.text.ParseException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class PriceController {
     private LocalTime start_time, end_time;
@@ -18,7 +20,7 @@ public class PriceController {
 
     }
 
-    public double getPrice() throws ParseException {
+    public double getPrice(String carparkId) throws ParseException {
         Scanner sc = new Scanner(System.in);
         DateTimeFormatter Dateformatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter Timeformatter = DateTimeFormatter.ofPattern("HH:mm");
@@ -28,6 +30,7 @@ public class PriceController {
         LocalTime end_time = null;
         String start_date_input = "", end_date_input = "";
         String start_time_input = "", end_time_input = "";
+        Boolean restricted = false;
 
         //Get date inputs
         
@@ -109,31 +112,103 @@ public class PriceController {
         int end_min_2_int = end_min_2;
         start_min_2_int -= 48;
 
-        //Get rate
-        double rate = 0.02; // 2 cents a min for non restricted
-        if(start_hour_1_int == 0 && start_hour_2_int > 7 && end_hour_1_int == 1 && end_hour_2_int < 7) {
-            rate = 0.04;
+        //Get restricted
+        String line = " ";
+        String splitBy = ",";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("hdb-carpark-information.csv"));
+            while((line = br.readLine()) != null) {
+                String[] carpark = line.split(splitBy);
+                String number = carpark[0];
+                String address = carpark[1];
+                String x_coord = carpark[2];
+                String y_coord = carpark[3];
+                String restrictedYN = carpark[4];
+                String Y = "Y";
+                if(number.equals(carparkId) && restrictedYN.equals(Y)) {
+                    restricted = true;
+                    break;
+                }
+            
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
         }
 
-        //Get time parked
-        int minutes_parked = 0;
-        int hours_only = 0;
-        int minutes_only = 0;
+        //Defining rates
+        double unrestricted_rate = 0.02; // 2 cents a min for non restricted
+        double restricted_rate = 0.04;
 
-        hours_only = ((10 * end_hour_1_int) + end_hour_2_int) - ((10 * start_hour_1_int) + start_hour_2_int);
+        //Get restricted and unrestricted parking minutes
+        int restricted_minutes_parked = 0;
+        int restricted_hours_only = 0;
+        int restricted_minutes_only = 0;
+        int unrestricted_minutes_parked = 0;
+        int unrestricted_hours_only = 0;
+        int unrestricted_minutes_only = 0;
+
+        if(restricted && start_date.getDayOfWeek() != DayOfWeek.SUNDAY) {
+            
+            //entire period is within restricted
+            if(start_hour_1_int == 0 && start_hour_2_int >= 7 && end_hour_1_int == 1 && end_hour_2_int < 7 ) {
+                restricted_hours_only = ((10 * end_hour_1_int) + end_hour_2_int) - ((10 * start_hour_1_int) + start_hour_2_int);
+                if(restricted_hours_only == 0) {
+                    restricted_minutes_only = ((10 * end_min_1_int) + end_min_2_int) - ((10 * start_min_1_int) + start_min_2_int);
+                } else if((10 * end_min_1_int) + end_min_2_int < (10 * start_min_1_int) + start_min_2_int){ //less than one hour difference in minutes
+                    restricted_minutes_only = ((10 * end_min_1_int) + end_min_2_int + 60) - ((10 * start_min_1_int) + start_min_2_int);
+                    restricted_hours_only -= 1;
+                } else {
+                    restricted_minutes_only = ((10 * end_min_1_int) + end_min_2_int) - ((10 * start_min_1_int) + start_min_2_int);
+                }
+                    restricted_minutes_parked = restricted_minutes_only + (60 * restricted_hours_only);
+            } else if(start_hour_1_int == 0 && start_hour_2_int < 7 && ((end_hour_1_int == 1 && end_hour_2_int < 7) || (end_hour_1_int == 0 && end_hour_2_int >= 7))) { //start before restricted
+                //Calculate unrestricted parking time
+                unrestricted_hours_only = 7 -  start_hour_2_int;
+                unrestricted_minutes_only = 60 - ((10 * start_min_1_int) + start_min_2_int);
+                unrestricted_minutes_parked = unrestricted_minutes_only + (60 * unrestricted_hours_only);
+                //Calculate restricted parking time
+                restricted_hours_only = ((10 * end_hour_1_int) + end_hour_2_int) - ((10 * start_hour_1_int) + start_hour_2_int) - unrestricted_hours_only;
+                restricted_minutes_only = ((10 * end_min_1_int) + end_min_2_int);
+                restricted_minutes_parked = restricted_minutes_only + (60 * restricted_hours_only);
+            } else if((start_hour_1_int == 0 && start_hour_2_int >= 7) || (start_hour_1_int == 1 && start_hour_2_int < 7) && (end_hour_1_int == 1 && end_hour_2_int >= 7)) { //end after restricted
+                //Calculate unrestricted parking time
+                unrestricted_hours_only = end_hour_2_int - 7;
+                unrestricted_minutes_only = (10 * end_min_1_int + end_min_2_int);
+                unrestricted_minutes_parked = unrestricted_minutes_only + (60 * unrestricted_hours_only);
+                //Calculate restricted parking time
+                restricted_hours_only = 17 - ((10 * start_hour_1_int) + start_hour_2_int) - unrestricted_hours_only;
+                restricted_minutes_only = 60 - ((10 * start_min_1_int) + start_min_2_int);
+                restricted_minutes_parked = restricted_minutes_only + (60 * restricted_hours_only);
+            } else { //start before restricted and end after restricted
+                restricted_hours_only = 10;
+                unrestricted_hours_only = ((10 * end_hour_1_int) + end_hour_2_int) - ((10 * start_hour_1_int) + start_hour_2_int) - restricted_hours_only;
+                if((10 * end_min_1_int) + end_min_2_int < (10 * start_min_1_int) + start_min_2_int) { //less than one hour difference in minutes
+                    unrestricted_minutes_only = ((10 * end_min_1_int) + end_min_2_int + 60) - ((10 * start_min_1_int) + start_min_2_int);
+                    unrestricted_hours_only -= 1;
+                } else {
+                    unrestricted_minutes_only = ((10 * end_min_1_int) + end_min_2_int) - ((10 * start_min_1_int) + start_min_2_int);
+                }
+                restricted_minutes_parked = restricted_minutes_only + (60 * restricted_hours_only);
+                unrestricted_minutes_parked = unrestricted_minutes_only + (60 * unrestricted_hours_only);
+            }
         
-        if(hours_only == 0) {
-            minutes_only = ((10 * end_min_1_int) + end_min_2_int) - ((10 * start_min_1_int) + start_min_2_int);
+    } else { //Not within restricted period or not within restricted area
+        unrestricted_hours_only = ((10 * end_hour_1_int) + end_hour_2_int) - ((10 * start_hour_1_int) + start_hour_2_int);
+        if(unrestricted_hours_only == 0) {
+            unrestricted_minutes_only = ((10 * end_min_1_int) + end_min_2_int) - ((10 * start_min_1_int) + start_min_2_int);
+        } else if((10 * end_min_1_int) + end_min_2_int < (10 * start_min_1_int) + start_min_2_int) {//less than one hour difference in minutes
+            unrestricted_minutes_only = ((10 * end_min_1_int) + end_min_2_int + 60) - ((10 * start_min_1_int) + start_min_2_int);
+            unrestricted_hours_only -= 1;
         } else {
-            minutes_only = ((10 * end_min_1_int) + end_min_2_int + 60) - ((10 * start_min_1_int) + start_min_2_int);
+            unrestricted_minutes_only = ((10 * end_min_1_int) + end_min_2_int) - ((10 * start_min_1_int) + start_min_2_int);
         }
-
-        minutes_parked = minutes_only + (60 * hours_only);
-
-        //Get price
-        double price = rate * minutes_parked;
-
-        return price;
+            unrestricted_minutes_parked = unrestricted_minutes_only + (60 * unrestricted_hours_only);
     }
+    //Get price
+    double unrestricted_price = unrestricted_rate * unrestricted_minutes_parked;
+    double restricted_price = restricted_rate * restricted_minutes_parked;
 
+    return unrestricted_price + restricted_price;
+    }
 }
