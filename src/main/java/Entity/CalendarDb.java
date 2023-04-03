a832c998b627ad073056c4a5ab3494a8c92177dd
Pareena Kaur <pareenakaur72@gmail.com>
2023-04-03 23:22:56 +0800

    Update CalendarDb.java

--- src/main/java/Entity/CalendarDb.java
+++ src/main/java/Entity/CalendarDb.java
@@ -1,39 +1,90 @@
 package Entity;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Set;
 import java.util.ArrayList;
-import de.jollyday.Holiday;
-import de.jollyday.HolidayCalendar;
-import de.jollyday.HolidayManager;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
- 
-public class CalendarDb {
-	private ArrayList<String> dateTypes = new ArrayList<String>();
-	private String countryCode = "SG";
-	
-	public ArrayList<String> dateType(LocalDate start_date, LocalDate end_date) {
-		HolidayManager holidayManager = HolidayManager.getInstance(HolidayCalendar.valueOf(countryCode));
-		int start_year = start_date.getYear();
-		int end_year = end_date.getYear();
-		Set<Holiday> holidays = holidayManager.getHolidays(start_year);
-		holidays.addAll(holidayManager.getHolidays(end_year));
-		System.out.println(holidays);
-		for(LocalDate date = start_date; date.isBefore(end_date); date.plusDays(1)) {
-			String day =  date.format(DateTimeFormatter.ofPattern("EEEE"));
-			for (Holiday holiday: holidays) {
-				if(date.equals(holiday.getDate())){
-					day = "Public Holiday";
-					break;
-				}
-			}
-			dateTypes.add(day);
-		}
-		return dateTypes;
-		
-	}
-	
-	
-	
+public class Calendar {
+	String apiKey = "820d182364437e8a58702c485705fd3417ea49f2";
+    	String country = "SG";
+    	String year = "2023";
+   	ArrayList<String> dateTypes = new ArrayList<String>();
+    
+    	public ArrayList<String> dateType(LocalDate start_date, LocalDate end_date) {
+        	ArrayList<String> holidaysList = new ArrayList<String>();
+        	try {
+            		// Create the URL for the API endpoint
+            		String urlString = "https://calendarific.com/api/v2/holidays?api_key=" + apiKey + "&country=" + country + "&year=" + year;
+            		URL url = new URL(urlString);
 
+            		// Open a connection to the URL using HttpURLConnection
+            		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            		// Set the request method to GET
+            		connection.setRequestMethod("GET");
+
+            		// Get the response code
+            		int responseCode = connection.getResponseCode();
+            		// Check if the response code indicates success
+            		if (responseCode == HttpURLConnection.HTTP_OK) {
+                		// Read the response data from the connection's input stream
+               			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                		String line;
+                		StringBuffer response = new StringBuffer();
+                
+                		while ((line = reader.readLine()) != null) {
+                    			response.append(line);
+                		}
+                		reader.close();
+
+                		JSONObject jsonResponse = new JSONObject(response.toString());
+                		JSONArray holidaysArray = jsonResponse.getJSONObject("response").getJSONArray("holidays");
+                    
+                		for (int i = 0; i < holidaysArray.length(); i++) {
+                    			JSONObject holidayObject = holidaysArray.getJSONObject(i);
+                    			String holidayName = holidayObject.getString("name");
+                    			String holidayDateIsoStr = holidayObject.getJSONObject("date").getString("iso");
+                    			String holidayDateStr = holidayDateIsoStr.substring(0, 10);
+                    			DateTimeFormatter holidayFormatter = DateTimeFormatter.ofPattern("uuuu-MM-dd");
+                    			LocalDate holidayDate = LocalDate.parse(holidayDateStr, holidayFormatter);
+                    			holidaysList.add(holidayDate + ":" + holidayName);
+            	    		}
+            		} else {
+               		System.out.println("GET request failed: " + responseCode);
+            }
+            for (LocalDate date = start_date; date.isBefore(end_date.plusDays(1)); date = date.plusDays(1)) {
+                String day = date.format(DateTimeFormatter.ofPattern("EEEE"));
+                boolean isHoliday = false;
+                String holidayName = "";
+
+                for (String holiday : holidaysList) {
+                    DateTimeFormatter holidayformatter = DateTimeFormatter.ofPattern("uuuu-MM-dd");
+                    LocalDate holidayDate = LocalDate.parse(holiday.split(":")[0], holidayformatter);
+                    if (date.equals(holidayDate)) {
+                        isHoliday = true;
+                        holidayName = holiday.split(":")[1];
+                        break;
+                    } 
+                }
+
+                if (isHoliday) {
+                    dateTypes.add("Public Holiday");
+                    //System.out.println(holidayName);
+                } else {
+                    dateTypes.add(day);
+                    //System.out.println(day);
+                }
+            }
+            
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dateTypes;
+    }
 }
